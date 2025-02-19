1f9f70f0261636e9bcd976942993bd5f849f7716
Corina Miner <c.miner19@gmail.com>
2025-02-19 02:03:27 -0500

    big styling/usability pass

--- webapp/utils.js
+++ webapp/utils.js
@@ -9,30 +9,37 @@ export class Point {
     }
 }
 
-const WHITE = "255 255 255";
-const BLACK = "0 0 0";
-const GREY = "210 210 210";
+// Piece colors
 const RED = "255 0 0";
 const GREEN = "0 255 0";
 const BLUE = "0 0 255";
+const WHITE = "255 255 255";
 const ORANGE = "255 170 0";
 const YELLOW = "255 255 0";
+// Colors for other elements
+const BLACK = "0 0 0";
+const LIGHT_GREY = "190 190 190";
+const DARK_GREY = "110 110 110";
+const DARK_BKGD = "60 60 68";
+const LIGHT_BKGD = "240 240 220";
 
 class Colors {
     constructor(darkMode) {
         this.darkMode = darkMode;
-        this.setColors();
-    }
-    setColors() {
-        this.backgrd = this.darkMode ? BLACK : WHITE;
-        this.gridlines = GREY;
-        this.puzzleBorder = this.darkMode ? WHITE : BLACK;
+        // colors that do not change between dark mode and light mode
         this.red = RED;
         this.green = GREEN;
         this.blue = BLUE;
         this.white = WHITE;
         this.orange = ORANGE;
         this.yellow = YELLOW;
+        this.pieceBorder = BLACK;
+        this.setColors();
+    }
+    setColors() {
+        this.backgrd = this.darkMode ? DARK_BKGD : LIGHT_BKGD;
+        this.gridlines = this.darkMode ? DARK_GREY : LIGHT_GREY;
+        this.puzzleBorder = this.darkMode ? LIGHT_GREY : DARK_GREY;
     }
     switch() {
         this.darkMode = !this.darkMode;
